1af1858a5d104cf68859063b23002cb26d8651c8
vaibdix <dixitvaib@gmail.com>
2023-07-07 02:14:47 +0530

    googleauth added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
+import Button from "@/components/ui/Button"
 import { db } from "@/lib/db"
 
 export default async function Home() {
 
 
   return (
-    <div className='text-red-500'>
-    hii
-    </div>
+    <Button>Hello</Button>
   )
 }
